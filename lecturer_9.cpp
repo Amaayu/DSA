02f280557a078829e06030b399f99ad2ca4a76a6
Amaayu <namdevaniket446@gmail.com>
2024-11-13 00:50:38 +0530

    done

--- lecturer_9.cpp
+++ lecturer_9.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
+int linearSearch(vector<int> &vec, int target);
 int main()
 {
+  vector<int> vec = {4, 82, 45, 23, 98, 65, 76, 45};
+  int target = 45;
 
-    int age;
-    cout << "hello coders \n";
-    cin >> age;
-    return 0;
+  int value = linearSearch(vec, target);
+
+  cout << value << endl;
+}
+int linearSearch(vector<int> &vec, int target)
+{
+  short int a = 0;
+  for (int valu : vec)
+  {
+    a++;
+    if (valu == target)
+      return a - 1;
+  }
+  return -1;
 }
