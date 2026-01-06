07b43d87010fed909b1a912328daf2b7e84e58db
aayu <namdevaniket446@gmail.com>
2026-01-06 18:02:16 +0530

    this is done

---
+++ Compant_Task/Fibonacci_Series.java
@@ -0,0 +1,28 @@
+package Compant_Task;
+
+import java.util.Scanner;
+
+public class Fibonacci_Series {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter single digit number you want print fibonacci_Series" + "\n");
+        Integer Number = scan.nextInt();
+
+        int First = 0;
+        int Second = 1;
+        int Next ;
+
+        System.out.print("0,");
+
+        for (int i = 0 ; i < Number ; i++){
+
+          Next =   First + Second;
+          First =  Second;
+          Second = Next;
+
+            System.out.print(First +  ",");
+        }
+
+    }
+}
